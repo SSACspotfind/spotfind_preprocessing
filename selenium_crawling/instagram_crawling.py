b79086bf4ba31b359b79a6741f57a23ce95dc16f
TomasAQ <tomasaq0511@gmail.com>
2021-08-01 13:39:13 +0900

    MOD instargramsave_changeDir

--- selenium_crawling/instagram_crawling.py
+++ selenium_crawling/instagram_crawling.py
@@ -4,7 +4,6 @@ import time
 import urllib.request
 from resource_data.keys import Instagram_Id ,Instagram_Pw
 
-
 # 셀레니움 열기
 driver = webdriver.Chrome()
 # 구글 이미지 검색 주소
@@ -20,6 +19,7 @@ time.sleep(3)
 # 알람설정 나중에 
 driver.find_element_by_css_selector(".aOOlW.HoLwm").click()
 time.sleep(1)
+
 # 키워드 검색
 searchName = "공주 송산리 고분군"
 search_keyword = driver.find_element_by_css_selector(".XTCLo.x3qfX")
@@ -32,13 +32,13 @@ down_imgCount = 5
 img_count = 1
 
 instDatas = driver.find_elements_by_css_selector(".v1Nh3.kIKUG._bz0w")
+
 for instData in instDatas :
     try:
         if(img_count == down_imgCount+1):
             break
         imgUrl = instData.find_element_by_css_selector(".FFVAD").get_attribute("src")
-        # urllib.request.urlretrieve(imgUrl, '../imgs/tourapi_img/' + searchName + '/' + searchName + str(img_count) + "_instr.jpg")
-        urllib.request.urlretrieve(imgUrl, searchName + str(img_count) + "_instr.jpg")
+        urllib.request.urlretrieve(imgUrl,'../imgs/tourapi_img/'+ searchName +'/'+ searchName + str(img_count) + "_instr.jpg")
         img_count = img_count + 1
     except :
         pass
