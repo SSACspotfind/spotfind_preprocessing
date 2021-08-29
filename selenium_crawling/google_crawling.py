49b7eef80e03f171b9f35f781e9eab40bfb54584
TomasAQ <tomasaq0511@gmail.com>
2021-08-29 12:20:54 +0900

    modify 지역별 관광지 이미지 크롤링

--- selenium_crawling/google_crawling.py
+++ selenium_crawling/google_crawling.py
@@ -12,9 +12,11 @@ import os
 # down_imgCount : 다운로드할 이미지수
 # alldata : 페이지 전체 스크롤 여부
 def googleImageSearch(searchName , down_imgCount , alldata ):
+    global Gdriver
+    Gdriver = webdriver.Chrome()
     # 구글 이미지 검색 주소
-    driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
-    elem = driver.find_element_by_name("q")
+    Gdriver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
+    elem = Gdriver.find_element_by_name("q")
     # 검색어 입력
     elem.send_keys(searchName)
     # 엔터키
@@ -31,25 +33,29 @@ def googleImageSearch(searchName , down_imgCount , alldata ):
 # searchName : 다운받을 이미지명
 # down_imgCount : 다운로드할 이미지수
 def imgDownload(searchName , down_imgCount):
-
-    images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+    images = Gdriver.find_elements_by_css_selector(".rg_i.Q4LuWd")
     img_count = 1
+
     for image in images :
         if(img_count == down_imgCount+1):
-            break;
+            break
         try:
             image.click()
-            time.sleep(2)
+            time.sleep(0.5)
 
-            imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
+            imgUrl = Gdriver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
             # 폴더 생성하기
 
             os.makedirs('../imgs/tourapi_img/' + searchName + '/', exist_ok=True)
             # 다운로드
             urllib.request.urlretrieve(imgUrl,'../imgs/tourapi_img/'+searchName+'/'+ searchName +str(img_count)+".jpg")
             img_count = img_count + 1
-        except :
-            pass
+        except BaseException as e:
+            print(e)
+
+
+    Gdriver.close()
+
 
 
 
@@ -57,28 +63,28 @@ def imgDownload(searchName , down_imgCount):
 def allimage():
     # 스크롤
     # 자바스크립트로 스크롤 크기를 찾아서 저장
-    last_height = driver.execute_script("return document.body.scrollHeight")
+    last_height = Gdriver.execute_script("return document.body.scrollHeight")
 
     while True:
         # 브라우저 끝까지 스크롤 내림
-        driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+        Gdriver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
 
         time.sleep(1)
 
-        new_height = driver.execute_script("return document.body.scrollHeight")
+        new_height = Gdriver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             try:
                 #driver.find_element_by_css_selector(".r0zKGf").click()
-                driver.find_element_by_css_selector(".mye4qd").click()
-            except:
-                break
+                Gdriver.find_element_by_css_selector(".mye4qd").click()
+            except BaseException as e:
+                print(e)
+
         last_height = new_height
 
-if __name__ == '__main__':
-    # 셀레니움 열기
-    driver = webdriver.Chrome()
-    # 검색어명 , 다룬로드 사진수 , 전체 데이터 스크롤 여부
-    googleImageSearch("공주 금성동 유적" , 10 , False)
-    googleImageSearch("공주 송산리 고분군", 10, False)
-
-    driver.close()
+# if __name__ == '__main__':
+#     # 셀레니움 열기
+#     # 검색어명 , 다룬로드 사진수 , 전체 데이터 스크롤 여부
+#     googleImageSearch("공주 금성동 유적" , 10 , False)
+#     googleImageSearch("공주 송산리 고분군", 10, False)
+#
+#     driver.close()
--- selenium_crawling/instagram_crawling.py
+++ selenium_crawling/instagram_crawling.py
@@ -4,42 +4,60 @@ import time
 import urllib.request
 from resource_data.keys import Instagram_Id ,Instagram_Pw
 
-# 셀레니움 열기
-driver = webdriver.Chrome()
-# 구글 이미지 검색 주소
-driver.get("https://www.instagram.com/")
-time.sleep(0.5)
-# 로그인 페이지 
-elem = driver.find_element_by_name("username")
-elem.send_keys(Instagram_Id)
-elem = driver.find_element_by_name("password")
-elem.send_keys(Instagram_Pw)
-elem.send_keys(Keys.RETURN)
-time.sleep(3)
-# 알람설정 나중에 
-driver.find_element_by_css_selector(".aOOlW.HoLwm").click()
-time.sleep(1)
 
-# 키워드 검색
-searchName = "공주 송산리 고분군"
-search_keyword = driver.find_element_by_css_selector(".XTCLo.x3qfX")
-search_keyword.send_keys(searchName)
-time.sleep(1)
-search_keyword = driver.find_element_by_css_selector(".-qQT3").click()
-time.sleep(5)
+def instagramGetImg(searchKeyword,selectElement, getImgCount):
+    # 셀레니움 열기
+    driver = webdriver.Chrome()
+    driver.get("https://www.instagram.com/")
+    time.sleep(0.5)
+    # 로그인 페이지
+    elem = driver.find_element_by_name("username")
+    elem.send_keys(Instagram_Id)
+    elem = driver.find_element_by_name("password")
+    elem.send_keys(Instagram_Pw)
+    elem.send_keys(Keys.RETURN)
+    time.sleep(3)
+    # 로그인 정보 저장
+    driver.find_element_by_css_selector(".sqdOP.yWX7d.y3zKF").click()
+    time.sleep(3)
+    # 알람설정 나중에
+    driver.find_element_by_css_selector(".aOOlW.HoLwm").click()
+    time.sleep(1)
 
-down_imgCount = 5
-img_count = 1
+    # 키워드 검색
+    searchName = searchKeyword
+    search_keyword = driver.find_element_by_css_selector(".XTCLo.x3qfX")
+    search_keyword.send_keys(searchName)
+    time.sleep(2)
+    search_element = driver.find_elements_by_css_selector(".-qQT3")
 
-instDatas = driver.find_elements_by_css_selector(".v1Nh3.kIKUG._bz0w")
+    # 검색 키워드 중에 원하는 번째 키워드를 선택
+    elementCout =0
 
-for instData in instDatas :
-    try:
-        if(img_count == down_imgCount+1):
+    for i , clickElement in enumerate(search_element):
+
+        if(elementCout == i):
+            clickElement.click()
             break
-        imgUrl = instData.find_element_by_css_selector(".FFVAD").get_attribute("src")
-        urllib.request.urlretrieve(imgUrl,'../imgs/tourapi_img/'+ searchName +'/'+ searchName + str(img_count) + "_instr.jpg")
-        img_count = img_count + 1
-    except :
-        pass
-        print("except")
+
+    time.sleep(7)
+
+    down_imgCount = getImgCount
+    img_count = 1
+
+    instDatas = driver.find_elements_by_css_selector(".v1Nh3.kIKUG._bz0w")
+
+    for instData in instDatas:
+        try:
+            if (img_count == down_imgCount + 1):
+                break
+            imgUrl = instData.find_element_by_css_selector(".FFVAD").get_attribute("src")
+            urllib.request.urlretrieve(imgUrl, '../imgs/tourapi_img/' + searchName + '/' + searchName + str(
+                img_count) + "_instr.jpg")
+            img_count = img_count + 1
+        except BaseException as e:
+            print(e)
+
+
+    driver.close()
+
